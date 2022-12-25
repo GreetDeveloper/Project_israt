1774fcdf2e3ed3cd6622c626a1589a26a815612c
GreetDeveloper <bahadoriborz@gmail.com>
2022-12-25 15:40:06 +0100

    added localhost

--- client/index.js
+++ client/index.js
@@ -89,7 +89,7 @@ const handleSubmit = async(e)=>{
     loader(message_div);
 
     //fetch data from the israt server -> ISRAT response
-    const response = await fetch('http://locathost:5000',{
+    const response = await fetch('http://localhost:5000',{
 
         method: 'POST',
         headers: {
